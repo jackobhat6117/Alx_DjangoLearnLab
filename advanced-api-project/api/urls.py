ca3934ab316cc9c82072d283708b9994440e2117
eyob.teklu <eyob.teklu@safaricom.et>
2024-09-09 11:51:43 +0300

    fixing error

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,6 +4,7 @@ from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView,
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),               # List all books and create new book
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),   # Retrieve a book by ID
+    path('books/create/', BookCreateView.as_view(), name='book-create'),     # Create a new book (without pk in URL)
     path('books/update/', BookUpdateView.as_view(), name='book-update'),     # Update a book (without pk in URL)
     path('books/delete/', BookDeleteView.as_view(), name='book-delete'),     # Delete a book (without pk in URL)
 ]
