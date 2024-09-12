e5e2058a3750d4d1fa63dc31f73ab4b03689e220
jackobhat <jackobhat911@gmail.com>
2024-09-12 18:23:42 +0300

    fixing databases inside setting

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog',
+        'USERNAME': 'root',
+        'PASSWORD': 'Eyob@611730',
+        'PORT': '3306'
     }
 }
 
