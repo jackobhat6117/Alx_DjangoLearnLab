cfc99cda4c4dc0db58abdbf72c8d0f0cb4ab1e4a
eyob.teklu <eyob.teklu@safaricom.et>
2024-09-15 08:57:05 +0300

    updating with exercise 03

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Post
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -15,3 +16,7 @@ class CustomUserCreationForm(UserCreationForm):
         if commite:
             user.save()
         return user
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
