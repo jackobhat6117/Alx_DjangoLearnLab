e595e0dbbe026c5b0fe113735a0fcce1526af9b5
jackobhat <jackobhat911@gmail.com>
2024-09-16 23:24:36 +0300

    pushing social media app

---
+++ social_media_api/accounts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, User
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+
+    def __str__(self) -> str:
+        return self.username
+
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer, LoginSerializer
+from rest_framework import status
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+
+    def post(self, request):
+        serializer = LoginSerializer(data = request.data)
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
